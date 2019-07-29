efeadbd0bcc2dacfed8da83e25f573a93ebdb0c8
Iliyana Dimova <iliyanadimova98@gmail.com>
2019-07-29 15:15:40 +0300

    Added all GuestService functions and tests for them (updateGuestById fails)

--- src/main/java/eu/deltasource/internship/hotel/service/GuestService.java
+++ src/main/java/eu/deltasource/internship/hotel/service/GuestService.java
@@ -3,7 +3,9 @@ package eu.deltasource.internship.hotel.service;
 import eu.deltasource.internship.hotel.domain.Gender;
 import eu.deltasource.internship.hotel.domain.Guest;
 import eu.deltasource.internship.hotel.exception.FailedInitializationException;
+import eu.deltasource.internship.hotel.exception.ItemNotFoundException;
 import eu.deltasource.internship.hotel.repository.GuestRepository;
+import org.junit.internal.runners.statements.Fail;
 
 import java.util.List;
 
@@ -18,21 +20,57 @@ public class GuestService {
         this.guestRepository = guestRepository;
     }
 
-    public void createGuest(Guest guest){
-        if(!guestRepository.existsById(guest.getGuestId())){
-            guestRepository.save(guest);
-        }
-        else{
-            throw new FailedInitializationException("Guest already exists!");
-        }
-    }
-
-    public void createGuest(String firstName, String lastName, Gender gender){
+    /**
+     * Adds a new guest to GuestRepository
+     *
+     * @param firstName
+     * @param lastName
+     * @param gender
+     */
+    public void createGuest(String firstName, String lastName, Gender gender) {
         Guest newGuest = new Guest(0, firstName, lastName, gender);
         guestRepository.save(newGuest);
     }
 
-    public List<Guest> returnAllGuests(){
+    /**
+     * @return A list of all guests in a GuestRepository
+     */
+    public List<Guest> returnAllGuests() {
         return guestRepository.findAll();
+    }
+
+    /**
+     * Returns a guest by given guest ID
+     * Throws Exception if Guest with this ID doesn't exist!
+     *
+     * @param guestId given guest ID
+     * @return The guest wit this ID
+     */
+    public Guest returnGuestById(int guestId) {
+        return guestRepository.findById(guestId);
+    }
+
+    /**
+     * Updates a guest's name and gender by given guest ID
+     * Throws Exception if Guest with this ID doesn't exist!
+     *
+     * @param guestId   the ID of the guest we want to update
+     * @param firstName to be updated
+     * @param lastName  to be updated
+     * @param gender    to be updated
+     */
+    public void updateGuestById(int guestId, String firstName, String lastName, Gender gender) {
+        Guest updatedGuest = new Guest(guestId, firstName, lastName, gender);
+        guestRepository.updateGuest(updatedGuest);
+    }
+
+    /**
+     * Removes a guest by ID
+     * Throws Exception if Guest with this ID doesn't exist!
+     *
+     * @param guestId the ID of the guest we want to remove
+     */
+    public void removeGuestById(int guestId) {
+        guestRepository.delete(returnGuestById(guestId));
     }
 }
