9187991725b67c1e13bc186a21407b053413ff7d
Iliyana Dimova <iliyanadimova98@gmail.com>
2019-07-30 13:43:38 +0300

    Added RestController

--- src/main/java/eu/deltasource/internship/hotel/controllers/GuestControllers.java
+++ src/main/java/eu/deltasource/internship/hotel/controllers/GuestControllers.java
@@ -1,10 +1,24 @@
 package eu.deltasource.internship.hotel.controllers;
 
+import eu.deltasource.internship.hotel.domain.Guest;
+import eu.deltasource.internship.hotel.service.GuestService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping()
+import java.awt.print.Book;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+@RequestMapping("guests")
 
 public class GuestControllers {
+    GuestService guestService = new GuestService();
+
+    @RequestMapping(value = "/{id}", method = GET)
+    @ResponseBody
+    public Guest returnGuestById(@PathVariable int id){
+        return guestService.returnGuestById(id);
+    }
+
 }
