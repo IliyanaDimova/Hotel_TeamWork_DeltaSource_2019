029ab41cde10a45228e5c6ead8425e87356e48fe
mmihovski <mmihovski@gmail.com>
2019-07-30 14:34:08 +0300

    add room controller methods

--- src/main/java/eu/deltasource/internship/hotel/controllers/RoomControllers.java
+++ src/main/java/eu/deltasource/internship/hotel/controllers/RoomControllers.java
@@ -1,19 +1,40 @@
 package eu.deltasource.internship.hotel.controllers;
 
+import eu.deltasource.internship.hotel.domain.Room;
 import eu.deltasource.internship.hotel.repository.RoomRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import eu.deltasource.internship.hotel.service.RoomService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("rooms")
 
 public class RoomControllers {
 
-    private final RoomRepository roomRepository;
+    private final RoomService roomService;
 
-    public RoomControllers(RoomRepository roomRepository) {
-        this.roomRepository = roomRepository;
+    public RoomControllers(RoomService roomService) {
+        this.roomService = roomService;
     }
-    
+
+    @GetMapping("/rooms")
+    List<Room> getAllRooms() {
+        return roomService.findRooms();
+    }
+
+    @PostMapping("/rooms")
+    Room addNewRoom(@RequestBody Room newRoom) {
+        return roomService.saveRoom(newRoom);
+    }
+
+    @PostMapping("/rooms")
+    void addNewRoom(@RequestBody Room... rooms) {
+        roomService.saveRooms(rooms);
+    }
+
+    @GetMapping("/rooms/{id}")
+    Room getRoomById(@PathVariable int id){
+        return roomService.getRoomById(id);
+    }
 }
