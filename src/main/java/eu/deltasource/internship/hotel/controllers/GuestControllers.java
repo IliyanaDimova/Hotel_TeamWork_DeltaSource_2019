3589266f1b90e2c71dad4d3dd95d4d3705da2589
Iliyana Dimova <iliyanadimova98@gmail.com>
2019-07-30 15:35:06 +0300

    Ctrl Alt L

--- src/main/java/eu/deltasource/internship/hotel/controllers/GuestControllers.java
+++ src/main/java/eu/deltasource/internship/hotel/controllers/GuestControllers.java
@@ -13,36 +13,36 @@ import static org.springframework.web.bind.annotation.RequestMethod.*;
 @RestController
 @RequestMapping("guests")
 public class GuestControllers {
-    GuestRepository guestRepository = new GuestRepository();
-    GuestService guestService = new GuestService(guestRepository);
+	GuestRepository guestRepository = new GuestRepository();
+	GuestService guestService = new GuestService(guestRepository);
 
-    @GetMapping
-    @ResponseBody
-    public List<Guest> returnAllGuests(){
-        return guestService.returnAllGuests();
-    }
+	@GetMapping
+	@ResponseBody
+	public List<Guest> returnAllGuests() {
+		return guestService.returnAllGuests();
+	}
 
-    @PostMapping
-    @ResponseBody
-    public void returnCreateGuest(@PathVariable String firstName, @PathVariable String lastName, @PathVariable Gender gender){
-        guestService.createGuest(firstName, lastName, gender);
-    }
+	@PostMapping
+	@ResponseBody
+	public void returnCreateGuest(@PathVariable String firstName, @PathVariable String lastName, @PathVariable Gender gender) {
+		guestService.createGuest(firstName, lastName, gender);
+	}
 
-    @RequestMapping(value = "/{id}", method = GET)
-    @ResponseBody
-    public void returnGuestById(@PathVariable int id){
-        guestService.returnGuestById(id);
-    }
+	@RequestMapping(value = "/{id}", method = GET)
+	@ResponseBody
+	public void returnGuestById(@PathVariable int id) {
+		guestService.returnGuestById(id);
+	}
 
 	@RequestMapping(value = "/{id}", method = DELETE)
 	@ResponseBody
-	public void returnRemoveGuestById(@PathVariable int id){
+	public void returnRemoveGuestById(@PathVariable int id) {
 		guestService.removeGuestById(id);
 	}
 
 	@RequestMapping(value = "/{id}", method = POST)
 	@ResponseBody
-	public void returnUpdateGuestById(@PathVariable int id, @PathVariable String firstName, @PathVariable String lastName, @PathVariable Gender gender){
+	public void returnUpdateGuestById(@PathVariable int id, @PathVariable String firstName, @PathVariable String lastName, @PathVariable Gender gender) {
 		guestService.updateGuestById(id, firstName, lastName, gender);
 	}
 }
--- src/main/java/eu/deltasource/internship/hotel/repository/GuestRepository.java
+++ src/main/java/eu/deltasource/internship/hotel/repository/GuestRepository.java
@@ -13,144 +13,144 @@ import java.util.List;
  */
 public class GuestRepository {
 
-    private final List<Guest> repository;
+	private final List<Guest> repository;
 
-    /**
-     * Default constructor, which initializes the repository
-     * as an empty ArrayList.
-     */
-    public GuestRepository() {
-        repository = new ArrayList<>();
-    }
+	/**
+	 * Default constructor, which initializes the repository
+	 * as an empty ArrayList.
+	 */
+	public GuestRepository() {
+		repository = new ArrayList<>();
+	}
 
-    /**
-     * Returns an unmodifiable list of all items
-     * currently in the repository.
-     */
-    public List<Guest> findAll() {
-        return Collections.unmodifiableList(repository);
-    }
+	/**
+	 * Returns an unmodifiable list of all items
+	 * currently in the repository.
+	 */
+	public List<Guest> findAll() {
+		return Collections.unmodifiableList(repository);
+	}
 
-    /**
-     * Method, which checks the repository if
-     * there is an item available with the given id.
-     * <p>
-     * Check this always, before using operations with id's.
-     */
-    public boolean existsById(int id) {
-        for (Guest item : repository) {
-            if (item.getGuestId() == id)
-                return true;
-        }
-        return false;
-    }
+	/**
+	 * Method, which checks the repository if
+	 * there is an item available with the given id.
+	 * <p>
+	 * Check this always, before using operations with id's.
+	 */
+	public boolean existsById(int id) {
+		for (Guest item : repository) {
+			if (item.getGuestId() == id)
+				return true;
+		}
+		return false;
+	}
 
-    /**
-     * Returns a copy of the item from the repository
-     * with the given Id.
-     */
-    public Guest findById(int id) {
-        for (Guest item : repository) {
-            if (item.getGuestId() == id)
-                return new Guest(item);
-        }
-        throw new ItemNotFoundException("A Guest with id: " + id + " was not found!");
-    }
+	/**
+	 * Returns a copy of the item from the repository
+	 * with the given Id.
+	 */
+	public Guest findById(int id) {
+		for (Guest item : repository) {
+			if (item.getGuestId() == id)
+				return new Guest(item);
+		}
+		throw new ItemNotFoundException("A Guest with id: " + id + " was not found!");
+	}
 
-    /**
-     * Saves the item in the repository with a new id
-     * using the item count in the repository
-     */
-    public void save(Guest item) {
-        Guest newGuest = new Guest(idGenerator(), item.getFirstName(), item.getLastName(), item.getGender());
-        repository.add(newGuest);
-    }
+	/**
+	 * Saves the item in the repository with a new id
+	 * using the item count in the repository
+	 */
+	public void save(Guest item) {
+		Guest newGuest = new Guest(idGenerator(), item.getFirstName(), item.getLastName(), item.getGender());
+		repository.add(newGuest);
+	}
 
-    /**
-     * Saves the list of items in the repository
-     */
-    public void saveAll(List<Guest> items) {
-        items.forEach(
-                this::save);
-    }
+	/**
+	 * Saves the list of items in the repository
+	 */
+	public void saveAll(List<Guest> items) {
+		items.forEach(
+			this::save);
+	}
 
-    /**
-     * Saves all given items in the repository
-     */
-    public void saveAll(Guest... items) {
-        saveAll(Arrays.asList(items));
-    }
+	/**
+	 * Saves all given items in the repository
+	 */
+	public void saveAll(Guest... items) {
+		saveAll(Arrays.asList(items));
+	}
 
-    /**
-     * Updates the names and gender of a given Guest
-     * All validations should be done in the service layer!!!
-     * <p>
-     * Returns a copy of the updated item
-     */
-    public Guest updateGuest(Guest item) {
-        for (Guest guest : repository) {
-            if (guest.getGuestId() == item.getGuestId()) {
-                guest.changeGender(item.getGender());
-                guest.changeFirstAndLastNames(item.getFirstName(), item.getLastName());
-                return new Guest(guest);
-            }
-        }
+	/**
+	 * Updates the names and gender of a given Guest
+	 * All validations should be done in the service layer!!!
+	 * <p>
+	 * Returns a copy of the updated item
+	 */
+	public Guest updateGuest(Guest item) {
+		for (Guest guest : repository) {
+			if (guest.getGuestId() == item.getGuestId()) {
+				guest.changeGender(item.getGender());
+				guest.changeFirstAndLastNames(item.getFirstName(), item.getLastName());
+				return new Guest(guest);
+			}
+		}
 
-        throw new ItemNotFoundException("Guest not found in repository!");
-    }
+		throw new ItemNotFoundException("Guest not found in repository!");
+	}
 
-    /**
-     * Removes an item from the repository
-     * by searching for an exact match.
-     * <p>
-     * Returns true if an exact match is and deleted,
-     * returns false if there's no match and the list is unchanged.
-     */
-    public boolean delete(Guest item) {
-        return repository.remove(item);
-    }
+	/**
+	 * Removes an item from the repository
+	 * by searching for an exact match.
+	 * <p>
+	 * Returns true if an exact match is and deleted,
+	 * returns false if there's no match and the list is unchanged.
+	 */
+	public boolean delete(Guest item) {
+		return repository.remove(item);
+	}
 
-    /**
-     * Removes the item in the repository
-     * matching the given id.
-     * <p>
-     * Returns true if there's a match and is deleted,
-     * returns false if there's no match and the list is unchanged.
-     */
-    public boolean deleteById(int id) {
-        for (Guest guest : repository) {
-            if (guest.getGuestId() == id) {
-                return delete(guest);
-            }
-        }
-        return false;
-    }
+	/**
+	 * Removes the item in the repository
+	 * matching the given id.
+	 * <p>
+	 * Returns true if there's a match and is deleted,
+	 * returns false if there's no match and the list is unchanged.
+	 */
+	public boolean deleteById(int id) {
+		for (Guest guest : repository) {
+			if (guest.getGuestId() == id) {
+				return delete(guest);
+			}
+		}
+		return false;
+	}
 
-    /**
-     * Deletes all items in the repository
-     */
-    public void deleteAll() {
-        repository.clear();
-    }
+	/**
+	 * Deletes all items in the repository
+	 */
+	public void deleteAll() {
+		repository.clear();
+	}
 
-    /**
-     * Returns the number of items left in the repository
-     */
-    public int count() {
-        return repository.size();
-    }
+	/**
+	 * Returns the number of items left in the repository
+	 */
+	public int count() {
+		return repository.size();
+	}
 
 
-    /**
-     * generates an ID for the guest
-     *
-     * @return generated ID
-     */
-    private int idGenerator() {
-        if (count() == 0) {
-            return count() + 1;
-        }
+	/**
+	 * generates an ID for the guest
+	 *
+	 * @return generated ID
+	 */
+	private int idGenerator() {
+		if (count() == 0) {
+			return count() + 1;
+		}
 
-        return repository.get(count() - 1).getGuestId() + 1;
-    }
+		return repository.get(count() - 1).getGuestId() + 1;
+	}
 }
