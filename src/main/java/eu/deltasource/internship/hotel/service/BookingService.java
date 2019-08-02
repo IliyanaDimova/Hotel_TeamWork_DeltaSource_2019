af612971d98c3f258722f1705d99aef78350e854
Dnokov <dnokov1997@gmail.com>
2019-08-03 00:07:42 +0300

    Added additional null checks in the update and create methods. Extracted the logic which checks, if a booking is overlapping with another booking of the current room into a method and changed the name of some methods to more suitable ones. Adding additional validations to the create and update methods.

--- src/main/java/eu/deltasource/internship/hotel/service/BookingService.java
+++ src/main/java/eu/deltasource/internship/hotel/service/BookingService.java
@@ -4,7 +4,6 @@ import eu.deltasource.internship.hotel.domain.Booking;
 import eu.deltasource.internship.hotel.domain.Room;
 import eu.deltasource.internship.hotel.repository.BookingRepository;
 import eu.deltasource.internship.hotel.to.BookingTO;
-import org.springframework.stereotype.Service;
 
 import java.security.InvalidParameterException;
 import java.time.LocalDate;
@@ -33,22 +32,23 @@ public class BookingService {
 	 * Adds a new entry to the repository, throws invalid param exception, if the booking is null or if the
 	 * booking has a from value which is greater than the to value
 	 *
-	 * @param bookingId 	id of the booking
 	 * @param bookingTO 	id of the guest
 	 */
-	public void createBooking(int bookingId, BookingTO bookingTO) {
+	public void createBooking(BookingTO bookingTO) {
 
 		validateBooking(bookingTO);
 
-		if (bookingRepository.existsById(bookingId)) {
+		if (bookingRepository.existsById(bookingTO.getBookingId())) {
 			throw new InvalidParameterException("Booking with this ID already exists.");
 		}
+
+		validateRoom(bookingTO);
 
 		Room room = roomService.getRoomById(bookingTO.getRoomId());
 
 		//todo include in validate dates
 
-		bookingRepository.save(BookingTOtoModdel(bookingId, bookingTO));
+		bookingRepository.save(covertBookingTOtoBookingModel(bookingTO));
 	}
 
 	/**
@@ -67,6 +67,7 @@ public class BookingService {
 	 * @return the booking which has matching id
 	 */
 	public Booking getBookingById(int id) {
+		validateBookingID(id);
 		return bookingRepository.findById(id);
 	}
 
@@ -79,26 +80,27 @@ public class BookingService {
 	 * @return boolean depending if the item was successfully deleted or not
 	 */
 	public boolean removeBookingById(int id) {
-		if (!bookingRepository.existsById(id)) {
-			throw new InvalidParameterException("Booking with id does not exist");
-		}
+		validateBookingID(id);
 		return bookingRepository.deleteById(id);
 	}
 
 	//todo not working correctly, fix
 
 	/**
-	 * Updates a booking
+	 * Updates a booking by using it's ID and throws, if the booking is overlapping with another one
 	 *
 	 * @param id   id of the booking to be updated
 	 * @param from new from date
 	 * @param to   new to date
 	 */
 	public void updateBooking(int id, LocalDate from, LocalDate to) {
+		validateBookingID(id);
+
 		Booking book = bookingRepository.findById(id);
 
 		for (Booking booking : bookingRepository.findAll()) {
-			if (checkForOverlappingDates(from, to, booking) && booking.getRoomId() == book.getRoomId()) {
+			if (booking.getRoomId() == book.getRoomId() && isOverlapping(from, to, booking) && id
+				!= booking.getBookingId()) {
 				throw new InvalidParameterException("Booking overlaps with another one");
 			}
 		}
@@ -106,20 +108,37 @@ public class BookingService {
 		book.setBookingDates(from, to);
 	}
 
+	private void overlapChecker(BookingTO booking){
+		for (Booking current : bookingRepository.findAll()) {
+			if (current.getRoomId() == booking.getRoomId() && isOverlapping(booking.getFrom(),
+				booking.getTo(), current) && booking.getBookingId() == current.getBookingId()) {
+				throw new InvalidParameterException("Booking overlaps with another one");
+			}
+		}
+	}
 
-	private boolean checkForOverlappingDates(LocalDate from, LocalDate to, Booking book) {
+	private boolean isOverlapping(LocalDate from, LocalDate to, Booking book) {
 		return !(book.getFrom().isAfter(to) || book.getTo().isBefore(from) || book.getTo().equals(from));
 	}
 
 	private void validateBooking(BookingTO booking) {
-		validateDates(booking.getFrom(), booking.getTo());
+		if(booking == null){
+			throw new InvalidParameterException("Booking cannot be null");
+		} else if(booking.getFrom() == null) {
+			throw new InvalidParameterException("From date cannot be null");
+		} else if(booking.getTo() == null) {
+			throw new InvalidParameterException("To date cannot be null");
+		}
 
+		if(booking.getTo().isBefore(booking.getFrom())){
+			throw new InvalidParameterException("To date cannot be after from date.");
+		}
 
 		if (!guestService.existsById(booking.getGuestId())) {
 			throw new InvalidParameterException("Guest with this ID does not exist.");
 		}
 
-		if (!roomService.existsById(booking.getGuestId())) {
+		if (!roomService.existsById(booking.getRoomId())) {
 			throw new InvalidParameterException("Room does not exist.");
 		}
 
@@ -130,19 +149,10 @@ public class BookingService {
 		return room.getRoomCapacity() >= peopleCount;
 	}
 
-	private void validateDates(LocalDate from, LocalDate to) {
-		if(from == null) {
-			throw new InvalidParameterException("From date cannot be null");
-		}
-		else if(to == null) {
-			throw new InvalidParameterException("To date cannot be null");
-		}
-		if(to.isBefore(from)){
-			throw new InvalidParameterException("To date cannot be after from date.");
-		}
-
-	}
-
+	/**
+	 * Validates the room requested by the booking. Checks if the room exists, checks if the space in the room is enough
+	 * @param booking
+	 */
 	private void validateRoom(BookingTO booking){
 		if(!roomService.existsById(booking.getRoomId())){
 			throw new InvalidParameterException("Room does not exist.");
@@ -152,16 +162,19 @@ public class BookingService {
 			throw new InvalidParameterException("Not enough space in room");
 		}
 
-		for (Booking current : bookingRepository.findAll()) {
-			if (checkForOverlappingDates(booking.getFrom(), booking.getTo(), current) && current.getRoomId() != booking.getRoomId()) {
-				throw new InvalidParameterException("Booking overlaps with another one");
-			}
+
+	}
+
+	private void validateBookingID(int bookingID){
+		if(!bookingRepository.existsById(bookingID)){
+			throw new InvalidParameterException("Booking does not exist.");
 		}
 	}
 
-	private Booking BookingTOtoModdel(int id, BookingTO booking){
-		Booking book = new Booking(id, booking.getGuestId(), booking.getGuestId(), booking.getNumberOfPeople(),
-			booking.getFrom(), booking.getTo());
+	private Booking covertBookingTOtoBookingModel(BookingTO bookingTO){
+		Booking book = new Booking(bookingTO.getBookingId(), bookingTO.getGuestId(), bookingTO.getRoomId(),
+			bookingTO.getNumberOfPeople(),
+			bookingTO.getFrom(), bookingTO.getTo());
 		return book;
 	}
 
