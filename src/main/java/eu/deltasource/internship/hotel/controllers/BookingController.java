391ed6f7e4d2559781f4b0f4c109a4213de8aab4
Iliyana Dimova <iliyanadimova98@gmail.com>
2019-08-06 15:49:44 +0300

    double update booking problem

--- src/main/java/eu/deltasource/internship/hotel/controllers/BookingController.java
+++ src/main/java/eu/deltasource/internship/hotel/controllers/BookingController.java
@@ -1,12 +1,14 @@
 package eu.deltasource.internship.hotel.controllers;
 
 import eu.deltasource.internship.hotel.domain.Booking;
+import eu.deltasource.internship.hotel.exception.InvalidDateException;
 import eu.deltasource.internship.hotel.service.BookingService;
 import eu.deltasource.internship.hotel.to.BookingTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -38,11 +40,16 @@ public class BookingController {
 
 	@PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public void updateBooking(@RequestBody BookingTO bookingTO, @RequestParam int id) {
-		bookingService.updateBooking(id, bookingTO.getFrom(), bookingTO.getTo());
+		bookingService.updateBooking(bookingTO);
 	}
 
-	@PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
-	public void updateBooking(@RequestBody BookingTO bookingTO, @RequestParam int id) {
-		bookingService.updateBooking(id, bookingTO.getFrom(), bookingTO.getTo());
-	}
+	/*@PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public void updateBooking(@RequestBody List<LocalDate> fromTo, @RequestParam int id) {
+		if(fromTo.size()==2) {
+			bookingService.updateBooking(id, fromTo.get(0), fromTo.get(1));
+		}
+		else{
+			throw new InvalidDateException("You can't update with more than 2 dates - from and to!");
+		}
+	}*/
 }
