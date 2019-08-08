84c77c6a49189fb42382f5222cb6e832a0c04691
Dnokov <dnokov1997@gmail.com>
2019-08-08 09:58:09 +0300

    added more request to the booking controller

--- src/main/java/eu/deltasource/internship/hotel/service/BookingService.java
+++ src/main/java/eu/deltasource/internship/hotel/service/BookingService.java
@@ -47,7 +47,6 @@ public class BookingService {
 	public void createBooking(BookingTO bookingTO) {
 
 		validateBooking(bookingTO);
-
 		validateRoom(bookingTO);
 
 		creationOverlapChecker(bookingTO);
